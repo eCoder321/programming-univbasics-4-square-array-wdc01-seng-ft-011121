8b0cc33c2b8e0d88e581b54c91a65213b7544717
Fem Adisa <femiwonderful@gmail.com>
2020-11-12 07:51:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,14 @@
+require 'pry'
+
 def square_array(array)
   new_arr = Array.new
   count = 0
+  binding.pry
   while array[count] do
     new_arr.push((array[count] ** array[count]))
     count += 1
   end
+  binding.pry
   return new_arr
 end
 
